6077dc0ffdfcd35f794f981c37c644997725db64
cnzau <clement.nzau@gmail.com>
2016-05-07 20:30:06 +0300

    loops in templates

--- app/views.py
+++ app/views.py
@@ -4,9 +4,28 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Clement'} #placeholder user object(fake/mock object)
+    user = {'nickname': 'Clement'} #placeholder user object(fake/mock user object)
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        },
+        { 
+            'author': {'nickname': 'Timothy'}, 
+            'body': 'The ever evolving technology!' 
+        },
+        { 
+            'author': {'nickname': 'Nzau'}, 
+            'body': 'Music is a healing for the soul!' 
+        }
+    ]
     #displayed on the client's web browser
     #render_template invokes the Jinja2 templating engine
     #Jinja2 substitutes {{...}} blocks with the corresponding values provided as template arguments.
     return render_template('index.html',
-                           user=user)
+                           user=user,
+    	                   posts=posts)
