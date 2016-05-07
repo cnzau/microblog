a6615fbf55de5a0f97125b898d2811a4e5ec9839
cnzau <clement.nzau@gmail.com>
2016-05-07 21:44:08 +0300

    configuration for Flask-WTF extension which wraps the WTForms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+#set Flask to read and use the config file
+app.config.from_object('config')
 #Import at views the end and not the beginning of the script to avoid circular references.
 #The views module needs to import the app variable defined in this script.
 from app import views
---
+++ config.py
@@ -0,0 +1,4 @@
+#activates the cross-site request forgery prevention
+WTF_CSRF_ENABLED = True
+#cryptographic token used to validate a form
+SECRET_KEY = 'you-will-never-guess'
