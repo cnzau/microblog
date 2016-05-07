58b5eb0d2a1b14ec1b50ef1ca34211d2641c8496
cnzau <clement.nzau@gmail.com>
2016-05-07 11:55:38 +0300

    rendered the first template to the webpage

--- app/views.py
+++ app/views.py
@@ -1,17 +1,13 @@
+from flask import render_template
 from app import app
 #route decorators create the mappings from URLs / and /index to this function.
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Clemmo'} #placeholder user object(fake/mock object)
+    user = {'nickname': 'Clement'} #placeholder user object(fake/mock object)
     #displayed on the client's web browser
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    #render_template invokes the Jinja2 templating engine
+    #Jinja2 substitutes {{...}} blocks with the corresponding values provided as template arguments.
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
