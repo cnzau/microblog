6a6f663f48a01583e50d9cabf544fd3b639b65a3
cnzau <clement.nzau@gmail.com>
2016-05-08 06:57:44 +0300

    configure DB file path, folder to store data files, initialize db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #set Flask to read and use the config file
 app.config.from_object('config')
+#object that will be our database
+db = SQLAlchemy(app)
 #Import at views the end and not the beginning of the script to avoid circular references.
 #The views module needs to import the app variable defined in this script.
-from app import views
+#Import models module
+from app import views, models
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 #activates the cross-site request forgery prevention
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 #cryptographic token used to validate a form
 SECRET_KEY = 'you-will-never-guess'
 
@@ -9,3 +11,8 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+#path of our database file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#folder where we will store the SQLAlchemy-migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
